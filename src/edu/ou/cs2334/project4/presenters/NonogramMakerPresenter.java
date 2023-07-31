b1858da75449199671897f5edb03bfe841533fa6
Grace Lee <grace.lee-1@ou.edu>
2023-07-30 22:25:28 -0500

    Finished documentation

--- src/edu/ou/cs2334/project4/presenters/NonogramMakerPresenter.java
+++ src/edu/ou/cs2334/project4/presenters/NonogramMakerPresenter.java
@@ -1,13 +1,21 @@
 package edu.ou.cs2334.project4.presenters;
 
-import java.awt.Window;
+import javafx.stage.Window;
 import java.io.File;
-
 import edu.ou.cs2334.project4.interfaces.Openable;
 import edu.ou.cs2334.project4.interfaces.Saveable;
 import edu.ou.cs2334.project4.models.NonogramMakerModel;
 import edu.ou.cs2334.project4.views.NonogramMakerView;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.Pane;
+
+/**
+ * This class connects NonogramMakerModel and NonogramMaker view and presents them. This will be called
+ * in Main.
+ * @author Grace Lee
+ * @version 1.0
+ *
+ */
 
 public class NonogramMakerPresenter implements Openable, Saveable {
 	private NonogramMakerView view;
@@ -15,11 +23,14 @@ public class NonogramMakerPresenter implements Openable, Saveable {
 	
 	private int numRows;
 	private int numCols;
-	
-	
-	
 	private int cellLength;
 	
+	/**
+	 * Constructs a view and a model, and reads in their dimensions
+	 * @param numRows
+	 * @param numCols
+	 * @param cellLength
+	 */
 	public NonogramMakerPresenter(int numRows, int numCols, int cellLength) {
 		view = new NonogramMakerView(numRows, numCols, cellLength);
 		model = new NonogramMakerModel(numRows, numCols);
@@ -28,36 +39,75 @@ public class NonogramMakerPresenter implements Openable, Saveable {
 		this.numCols = numCols;
 		this.cellLength = cellLength;
 	}
-	
+	/**
+	 * Returns window of view
+	 * @return the Window of the Scene of the Pane of NonogramMakerView
+	 */
 	private Window getWindow() {
-		if()
-		return view.getPane();
+		return view.getPane().getScene().getWindow();
 	}
 	
+	/**
+	 * Initializes the togglebuttons and connects them with the model.
+	 */
 	private void init() {
 		
 	}
 	
+	/**
+	 * Call view.initButtons() with the field variable dimensions. If the window is not null, 
+	 * call .sizeToScene() so the window so the window is not too big/small
+	 */
 	private void initToggleButtons() {
-		
+		if(getWindow() != null) {
+			getWindow().sizeToScene();
+		}
 	}
 	
+	/**
+	 * Checks that each togglebutton's state matches the model's cell state, and adds an event
+	 * handler for each button.
+	 */
 	private void bindToggleButtons() {
+		for(int i = 0; i < (view.getNumRows()); ++i) {
+			for(int k = 0; k < view.getNumCols(); ++k) {
+				boolean modelCellState = model.getCell(i, k);
+				ToggleButton togBut = view.getToggleButton(i, k);
+				if(modelCellState) {
+					togBut.setSelected(true);
+				} else {
+					togBut.setSelected(false);
+				}
+			}
+		}
 		
 	}
 	
+	/**
+	 * Sets event handlers for the open and save buttons.
+	 */
 	private void configureMenuItems() {
 		
 	}
 	
+	/**
+	 * Returns the pane of the view.
+	 * @return view Pane
+	 */
 	public Pane getPane() {
-		
+		return view.getPane();
 	}
 	
+	/**
+	 * Calls the NonogramMakerModel constructor with the file parameter, and initializes togglebuttons
+	 */
 	public void open (File file) {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void save (String filename) {
 		
 	}
--- src/edu/ou/cs2334/project4/views/NonogramMakerView.java
+++ src/edu/ou/cs2334/project4/views/NonogramMakerView.java
@@ -15,6 +15,12 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
+/**
+ * Creates the entire GUI for the project. Adds a window and the File menu at the top.
+ * @author Grace Lee
+ * @version 1.0
+ */
+
 public class NonogramMakerView {
 	private BorderPane borderPane;
 	private MenuBar menuBar;
@@ -25,6 +31,12 @@ public class NonogramMakerView {
 	public final String MENU_ITEM_SAVE = "MENU_ITEM_SAVE";
 	public final String MENU_ITEM_EXIT = "MENU_ITEM_EXIT";
 	
+	/**
+	 * Initializes borderPane, menuBar, and cellGridView. Sets alignment for borderPane.
+	 * @param numRows
+	 * @param numCols
+	 * @param cellLength
+	 */
 	public NonogramMakerView (int numRows, int numCols, int cellLength) {
 		borderPane = new BorderPane();
 		cellGridView = new CellGridView(numRows, numCols, cellLength);
@@ -35,6 +47,9 @@ public class NonogramMakerView {
 		borderPane.setTop(menuBar);
 	}
 	
+	/**
+	 * Initializes menuBar and menuItemsMap. Adds open, exit, and save components to the menuBar.
+	 */
 	private void initMenuBar() {
 		Menu fileMenu = new Menu("_File");
 		MenuItem open = new MenuItem("_Open");
@@ -57,27 +72,56 @@ public class NonogramMakerView {
 		menuBar.getMenus().add(fileMenu);
 	}
 	
+	/**
+	 * Returns MenuItem
+	 * @param name
+	 * @return a menuItem from the menuItemsMap with given name
+	 */
 	public MenuItem getMenuItem(String name) {
 		
 		return menuItemsMap.get(name);
 	}
 	
+	/**
+	 * gets borderPane
+	 * @return borderPane
+	 */
 	public Pane getPane() {
 		return borderPane;
 	}
 	
+	/**
+	 * Initializes buttons by calling the initButtons method in cellGridView
+	 * @param numRows
+	 * @param numCols
+	 * @param cellLength
+	 */
 	public void initButtons(int numRows, int numCols, int cellLength) {
 		cellGridView.initButtons(numRows, numCols, cellLength);
 	}
 	
+	/**
+	 * Gets cellGridView.getToggleButton
+	 * @param row
+	 * @param col
+	 * @return getToggleButton method in cellGridView
+	 */
 	public ToggleButton getToggleButton(int row, int col) {
-		return new ToggleButton();
+		return cellGridView.getToggleButton(row, col);
 	}
 	
+	/**
+	 * Returns cellGridView.getNumRows
+	 * @return numRows in the cellGridView
+	 */
 	public int getNumRows() {
 		return cellGridView.getNumRows();
 	}
 	
+	/**
+	 * Returns cellGridView.getNumCols
+	 * @return numCols in the cellGridView
+	 */
 	public int getNumCols() {
 		return cellGridView.getNumCols();
 	}
